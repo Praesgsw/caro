aa7a16a7310120bd9cf3d6d1e328021d22358192
jake23n <114771334+jake23n@users.noreply.github.com>
2023-05-09 17:12:22 +0700

    caro.cpp

--- caro/caro.cpp
+++ caro/caro.cpp
@@ -1,10 +1,12 @@
 #include "Menu.h"
-
+#include "Board.h"
 int main() {
 
 	// full screen
-	SetConsole(1720, 1120);
+	system("color FA");
+	SetConsole(1120, 720);
 	//playSound(6);
+	LoadingGame();
 	menu();
 	system("pause");
 	return 0;
