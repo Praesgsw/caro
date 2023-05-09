807cf6f736cac92940abeba0ce244307f6fcf1e0
jake23n <114771334+jake23n@users.noreply.github.com>
2023-05-09 17:12:02 +0700

    Board.h

--- caro/Board.h
+++ caro/Board.h
@@ -1,6 +1,7 @@
-﻿#pragma once
+#pragma once
 #include "Menu.h"
-
+void Draw(int i, int x, int y);
+void LoadingGame();
 void drawBoard(); // Vẽ bảng caro
 void PrintScoreBoard(); //Vẽ bảng điểm
-void box(); //Hiển thị nút esc sau khi người dùng thắng
+void box(); //Hiển thị nút esc sau khi người dùng thắng#pragma once
