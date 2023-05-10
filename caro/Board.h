07fb08dff2d77819347d0d40d0e62f0da2e598cf
Praesgsw <132744478+Praesgsw@users.noreply.github.com>
2023-05-10 21:34:05 +0700

    Update Board.h

--- caro/Board.h
+++ caro/Board.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "Menu.h"
 
-void Draw(int i, int x, int y);
-void LoadingGame();
+void Draw(int i, int x, int y); //Vẽ thông thường
+void LoadingGame(); //Vẽ đồ họa màn hình intro
+void windraw(int i, int x, int y); //Vẽ đồ họa Win
 void drawBoard(); // Vẽ bảng caro
 void PrintScoreBoard(); //Vẽ bảng điểm
-void box(); //Hiển thị nút esc sau khi người dùng thắng#pragma once
+void box(); //Hiển thị nút esc sau khi người dùng thắng
